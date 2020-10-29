06b659e08499e525dc5b9317c2069c3b367f3ebd
alexConkey <aconkey20@stuy.edu>
2020-10-29 10:02:47 -0400

    tested getAccountID and toString

--- Tester.java
+++ Tester.java
@@ -3,5 +3,7 @@ public class Tester {
     BankAccount myAccount = new BankAccount(7, "Password123");
     myAccount.deposit(100.00);
     System.out.println(myAccount.getBalance());
+    System.out.println(myAccount.getAccountID());
+    System.out.println(myAccount.toString());
   }
 }
