e131d50dd792affd24659e44f09a25b40f0d7190
alexConkey <aconkey20@stuy.edu>
2020-10-29 09:38:05 -0400

    Mutiple set-up functions complete

--- BankAccount.java
+++ BankAccount.java
@@ -1,7 +1,21 @@
 public class BankAccount{
-  double balanceNum = 0.00;
-  static double balance(){
-    return balanceNum;
+  double balanceDouble = 0;
+  int accountInt = 0;
+  String passwordStr = "";
+  public BankAccount(int accountId, String password){
+    accountInt = accountId;
+    passwordStr = password;
   }
-  
+  public double getBalance(){
+    return balanceDouble;
+  }
+  public int getAccountID(){
+    return accountInt;
+  }
+  public void setPassword(String newPass){
+    passwordStr = newPass;
+  }
+  public boolean deposit(double ammount){
+    
+  }
 }
