829384a7578b8182e5eaa4be2bf0409b686e505f
alexConkey <aconkey20@stuy.edu>
2020-10-29 10:08:03 -0400

    Tests done

--- Tester.java
+++ Tester.java
@@ -2,6 +2,8 @@ public class Tester {
   public static void main(String[] args) {
     BankAccount myAccount = new BankAccount(7, "Password123");
     myAccount.deposit(100.00);
+    myAccount.withdraw(50.0);
+    myAccount.setPassword("NewPassword123");
     System.out.println(myAccount.getBalance());
     System.out.println(myAccount.getAccountID());
     System.out.println(myAccount.toString());
